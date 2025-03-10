10165c542018ea8bed2f026eccf229aa685bdca9
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-03-10 16:05:54 +0530

    wrapper, navbar responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,11 @@ import { Dumbbell } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Separator } from "./ui/separator";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 type Props = {};
 
@@ -27,24 +32,33 @@ const navLinks = [
 
 export default function Navbar({}: Props) {
   return (
-    <nav>
-      <div className="flex m-10 mb-5 justify-between items-center wrapper">
-        <h2 className="text-3xl flex gap-2 items-center font-semibold">
-          <Dumbbell />
-          RepMate
-        </h2>
-        <div className="flex items-center gap-10 font-semibold text-gray-400">
-          {navLinks.map((link) => (
-            <Link key={link.href} href={link.href}>
-              {link.name}
-            </Link>
-          ))}
+    <>
+      <nav className="wrapper">
+        <div className="flex m-10 mb-5 justify-between items-center ">
+          <h2 className="text-3xl flex gap-2 items-center font-semibold">
+            <Dumbbell />
+            Gymbro
+          </h2>
+          <div className="hidden md:flex items-center gap-10 font-semibold text-gray-400">
+            {navLinks.map((link) => (
+              <Link key={link.href} href={link.href}>
+                {link.name}
+              </Link>
+            ))}
+          </div>
+          <Link href={"/about"} className="hidden md:block">
+            <Button className="p-5 py-7 font-semibold cursor-pointer">
+              Join Waitlist
+            </Button>
+          </Link>
+
+          <Popover>
+            <PopoverTrigger>Open</PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
         </div>
-        <Link href={"/about"}>
-          <Button className="p-5 py-7 font-semibold">Join Waitlist</Button>
-        </Link>
-      </div>
+      </nav>
       <Separator />
-    </nav>
+    </>
   );
 }
