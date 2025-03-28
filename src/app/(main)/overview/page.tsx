42ea9824a0d25863083030f4e281c4ef86797d4d
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-03-28 23:16:32 +0530

    feat(add): add comments

--- src/app/(main)/overview/page.tsx
+++ src/app/(main)/overview/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type Props = {};
-
+//  make changes here add more components and other stuff to make it more nice
 export default function Page({}: Props) {
   return <div className="">this is dashboard</div>;
 }
