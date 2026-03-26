bd7d3a31f0e8d3ca06f379dd9da574f43be7d10a
ronakrpanchal <rhtpanchal76@gmail.com>
2026-03-26 14:40:21 +0530

    minor issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextJs from "eslint-config-next";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextJs,
+  {
+    rules: {
+      "react/display-name": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
